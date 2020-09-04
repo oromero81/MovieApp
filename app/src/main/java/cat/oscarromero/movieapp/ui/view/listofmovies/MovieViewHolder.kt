83f98e9bafcdd1f15398ff4ea4ffc64b426a27a9
Oscar Romero <meru3003@gmail.com>
2020-09-04 13:13:52 +0200

    Added list of movies image placeholder

--- app/src/main/java/cat/oscarromero/movieapp/ui/view/listofmovies/MovieViewHolder.kt
+++ app/src/main/java/cat/oscarromero/movieapp/ui/view/listofmovies/MovieViewHolder.kt
@@ -2,6 +2,7 @@ package cat.oscarromero.movieapp.ui.view.listofmovies
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import cat.oscarromero.movieapp.R
 import cat.oscarromero.movieapp.core.loadImageFromUrl
 import cat.oscarromero.movieapp.ui.model.MovieModel
 import kotlinx.android.synthetic.main.cell_movie.view.*
@@ -10,7 +11,11 @@ class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(movieModel: MovieModel, movieSelected: (MovieModel) -> Unit) {
         with(itemView) {
-            thumbnailImageView.loadImageFromUrl(movieModel.thumbnail)
+            thumbnailImageView.loadImageFromUrl(
+                movieModel.thumbnail,
+                R.drawable.ph_list_of_movies,
+                R.drawable.ph_list_of_movies
+            )
 
             setOnClickListener { movieSelected.invoke(movieModel) }
         }
