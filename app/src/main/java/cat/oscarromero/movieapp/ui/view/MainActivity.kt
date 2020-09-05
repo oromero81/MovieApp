c4dc696aa4faaab9121bf35d270e44c007e1d477
Oscar Romero <meru3003@gmail.com>
2020-09-06 00:03:01 +0200

    Custom toolbar

--- app/src/main/java/cat/oscarromero/movieapp/ui/view/MainActivity.kt
+++ app/src/main/java/cat/oscarromero/movieapp/ui/view/MainActivity.kt
@@ -1,9 +1,14 @@
 package cat.oscarromero.movieapp.ui.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import cat.oscarromero.movieapp.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -11,5 +16,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        topAppBar.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.movieDetailsFragment -> {
+                    appBarLayout.setExpanded(true, false)
+                    topAppBar.visibility = View.GONE
+                }
+                R.id.listOfMoviesFragment -> topAppBar.visibility = View.VISIBLE
+            }
+        }
     }
 }
